6da5b8b2deac4a00877cd2ee8dc1415f13c0486e
Anupam Kaul <anupam.kaul@yahoo.com>
2022-05-20 08:24:33 -0700

    RANDMAX does not need overwriting. A local variant brings it down to 4 digits.
    RANDMAX is an input in the algo, it does not modify the range of the target
    only dilutes it quality. Thus I don't need to override it.
    
    The mod with the max range makes it from 0 to max range so I used a while
    loop (unoptimal) to regenerate random number till it is from 1000 to 9999.
    
    Used while loop because I mostly observed that occurance of less than 4 digits
    was like 1 in 12 loops but actually should avoid the while and do it in 1 calc.

--- cowsnbulls/cowsnbulls.c
+++ cowsnbulls/cowsnbulls.c
@@ -7,17 +7,18 @@
  #define RAND_MAX 2147483647
 */
 
-#ifdef RAND_MAX
-#undef RAND_MAX
-#endif
-#define RAND_MAX 9999
+#define LOCAL_RAND_MAX 9999
 
 int main(int argc, char *argv[]) {
 
     printf("We are playing cows and bulls!\n");
 
     srand(time(NULL));
-    int target = rand();
+
+    int target = 999;
+    while (target <= 999) {
+        target = rand() % LOCAL_RAND_MAX; // starts from 0 to LRM
+    }
 
     printf("Target is %d (%d)\n", target, RAND_MAX);
     return 0;
