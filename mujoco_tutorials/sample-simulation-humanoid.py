5255913ca16c6cfa1f162371c624da2fd26da1dd
Anupam <anupam.kaul@yahoo.com>
2025-12-21 21:01:05 -0800

    mod the humanoid to stand still with arms moving
    disable freejoint and remove its 'falling' characteristic
    arms still move

--- mujoco_tutorials/sample-simulation-humanoid.py
+++ mujoco_tutorials/sample-simulation-humanoid.py
@@ -9,7 +9,8 @@ import os
 os.environ['MUJOCO_GL'] = 'egl' 
 
 # Define the path to your MuJoCo XML model file
-MODEL_PATH = './models/humanoid.xml' 
+#MODEL_PATH = './models/humanoid.xml' 
+MODEL_PATH = './models/humanoid-mod.xml' 
 
 # Simulation parameters
 DURATION = 3          # seconds
@@ -44,8 +45,31 @@ while data.time < DURATION:
     frames.append(pixels)
 
 # Save frames to a file using imageio
-output_video_path = 'simulation_humanoid_output.mp4'
+output_video_path = 'simulation_humanoid_mod_output.mp4'
 # Note: imageio might require ffmpeg as a backend (pip install imageio[ffmpeg]) 
 imageio.mimsave(output_video_path, frames, fps=FRAMERATE) 
 
 print(f"Simulation finished. Video saved to {output_video_path}")
+
+# read some data
+print("positions: ", data.qpos)
+print("velocities: ", data.qvel)
+
+'''
+21 element tuples returned. Do these correspond to 
+21 actuators as defined in the XML?
+
+positions:  [-1.51694072e-18 -9.93104121e-03 -2.31998066e-18 -1.23655526e-02
+  4.43190428e-04  9.37606251e-03 -3.70567596e-02  5.17926734e-02
+  3.12280701e-02 -1.23655526e-02  4.43190428e-04  9.37606251e-03
+ -3.70567596e-02  5.17926734e-02 -3.12280701e-02  6.88007926e-01
+ -5.18862221e-01 -1.57633831e+00 -6.88007926e-01  5.18862221e-01
+ -1.57633831e+00]
+velocities:  [ 4.47188711e-18 -1.19750377e-02 -2.91529275e-17 -8.90043276e-04
+  4.85193365e-05 -3.63659222e-03  5.75708195e-03 -2.30275476e-04
+ -2.12101260e-04 -8.90043276e-04  4.85193365e-05 -3.63659222e-03
+  5.75708195e-03 -2.30275476e-04  2.12101260e-04  5.74229970e-01
+ -1.25563848e+00 -1.01865879e-03 -5.74229970e-01  1.25563848e+00
+ -1.01865879e-03]
+
+'''
